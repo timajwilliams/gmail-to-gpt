19f6415b0bb0173f84c570d53f7952e3cb85be21
tim <timajwilliams@gmail.com>
2024-09-08 08:56:09 +0100

    fixing error

--- defaults.js
+++ defaults.js
@@ -1,7 +1,7 @@
 const defaultSettings = {
     prependText: `Summarise the content of the email using bullet points, and then create a short draft email response. Separate these sections with a horizontal line divider.
   
-  Do not provide any conversational information from you to me, just perform the tasks please`,
+Do not provide any conversational information from you to me, just perform the tasks please`,
     appendText: ""
   };
   
